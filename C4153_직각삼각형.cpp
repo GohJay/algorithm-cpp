99618f4d25f2325a810da1fb84d14ce8199d2239
KojaehyunGit <51254582+KojaehyunGit@users.noreply.github.com>
2022-05-10 01:30:24 +0900

    Update C4153_직각삼각형.cpp

--- C4153_직각삼각형.cpp
+++ C4153_직각삼각형.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void swap(int &a, int &b)
+{
+	a ^= b;
+	b ^= a;
+	a ^= b;
+}
 int main()
 {
 	int a, b, c;
@@ -8,21 +14,11 @@ int main()
 	{
 		scanf("%d %d %d", &a, &b, &c);
 		if (a + b + c == 0) break;
-		if (c < b)
-		{
-			temp = c;
-			c = b;
-			b = temp;
-		}
-		if (c < a)
-		{
-			temp = c;
-			c = a;
-			a = temp;
-		}
 		/*
 		직각 삼각형 공식 : 작은변 제곱 * 작은변 제곱 == 큰변 제곱
 		*/
+		if (b > c) swap(b, c);
+		if (a > c) swap(a, c);		
 		if ((a * a) + (b * b) == (c * c))
 			printf("%s\n", "right");
 		else
